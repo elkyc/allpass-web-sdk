ee2c33799d16ca156c237d4aa1aa4eda5e9e45dd
Виктор Плотников <viktor.plotnikov@murka.com>
2023-05-03 15:55:39 +0100

    all files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Allpass Javascript Library
---
+++ example/integration.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Viktor Plotnikov <viktorr.plotnikov@gmail.com>
+ * Date: 12/03/2023 23:09
+ */
+const appKey = 'YOUR APP KEY';
+const clientSession = 'Client session identifier in your system';
+
+(() => {
+  const allpassId = 'allpass';
+  /** UI actions */
+  const setElmsDisplay = (hide, show) => {
+    document.getElementById(hide).style.display = 'none';
+    document.getElementById(show).style.display = 'block';
+  }
+  const finishUI = () => {
+    setTimeout(() => {
+      setElmsDisplay(allpassId, 'start');
+      window.Allpass.close();
+    }, 10000);
+  };
+
+  /** event handlers */
+  const onLoad = (e) => {
+    console.log('onLoad', e);
+    setElmsDisplay('loader', allpassId);
+  };
+  const onRestart = (e) => {
+    console.log('onRestart', e);
+    setElmsDisplay('start', 'loader');
+  };
+  const onStart = (e) => {
+    console.log('onStart', e);
+  };
+  const onPassStep = (e) => {
+    console.log('onPassStep', e);
+  };
+  const onComplete = (e) => {
+    console.log('onComplete', e);
+    finishUI();
+  };
+  const onError = (e) => {
+    console.log('onError', e);
+    finishUI();
+  };
+
+  /** initialize Allpass SDK */
+  const init = () => {
+    window.Allpass.init({
+      onLoad,
+      onRestart,
+      onStart,
+      onPassStep,
+      onComplete,
+      onError,
+    }).restart(clientSession);
+  };
+
+  /** create Allpass library */
+  const script = document.createElement('script');
+  script.src = '../index.js';
+  script.async = true;
+  script.onload = () => init();
+  document.body.appendChild(script);
+
+  /** start verification process */
+  document.getElementById('start').onclick = async () => {
+    setElmsDisplay('start', 'loader');
+    await window.Allpass.start(appKey, clientSession);
+  };
+})();
